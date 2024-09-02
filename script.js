d17731f02f7a3682a283f711d12164c60ecaaaf6
Meldeniseiurato <melanydeniseiurato@gmail.com>
2024-09-02 20:15:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function Comprar() {
+    let respuesta= prompt ('Ingresa tu E-mail')
+}
+Comprar()
+
+let precios [70000,30000,75000,120000,40000,45000,30000,60000,50000,40000,135000,60000,55000]
+
+for(let i =0;<precios.length;i++){
+    consol.console.log(i);
+    
+}
